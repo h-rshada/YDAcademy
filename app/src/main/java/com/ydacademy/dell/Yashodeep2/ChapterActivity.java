00faa2bdaf27253c1c5dbd8bb7ab0e8ef0b06648
android <monalijagtap8@gmail.com>
2017-12-11 12:57:44 +0530

    Login code changed

--- app/src/main/java/com/ydacademy/dell/Yashodeep2/ChapterActivity.java
+++ app/src/main/java/com/ydacademy/dell/Yashodeep2/ChapterActivity.java
@@ -110,7 +110,7 @@ public class ChapterActivity extends AppCompatActivity {
                                        }
                                        loading.dismiss();
 
-                                       Log.d("Chapters", chapters.toString() + "");
+                                       //Log.d("Chapters", chapters.toString() + "");
                                        adapter = new ArrayAdapter(ChapterActivity.this, android.R.layout.simple_list_item_1, chapters);
                                        listChapters.setAdapter(adapter);
                                        adapter.notifyDataSetChanged();
